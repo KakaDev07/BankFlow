e6642ddf944a28edce7321110c57926b953c239f
KakaDev07 <kaka2015.github@gmail.com>
2026-08-28 17:37:54 -0300

    feat: add account management service

---
+++ src/services/BankService.java
@@ -0,0 +1,32 @@
+package services;
+import entities.BankAccount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BankService {
+
+    private List<BankAccount> accounts = new ArrayList<>();
+
+    public BankAccount findAccountByNumber(int number) {
+
+        for (BankAccount account : accounts) {
+            if (account.getNumber() == number) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public boolean addAccount(BankAccount account){
+        if(findAccountByNumber(account.getNumber()) != null){
+            return false;
+        }else{
+            accounts.add(account);
+                return true;
+            }
+        }
+
+    }
+
